54273db4bbbaaa67a25d2ec6c73276c07296fe1c
Calvin Huang <calvin.peak@capslock.tw>
2019-04-02 18:38:49 +0800

    feat(worker): add queue/worker.process
    The worker will subscribes on the subscription of pubsub and emit the messages to worker subscriber.

---
+++ src/worker.js
@@ -0,0 +1,66 @@
+/**
+ * Module dependencies
+ */
+const { EventEmitter } = require('events');
+const moment = require('moment');
+
+/**
+ * Utilities
+ */
+const logger = require('~utils/logger');
+const pubsub = require('~utils/pubsub');
+
+/**
+ * Configurations
+ */
+const { topicSuffix, subscriptionName } = require('~config/pubsub');
+
+class Worker extends EventEmitter {
+  constructor(config = {}, pubsubClient = pubsub) {
+    super();
+
+    this.config = config;
+    this.pubsub = pubsubClient;
+    this.subscriptions = {};
+  }
+
+  async getSubscription(topicName) {
+    // Establish the new subscription when it is not exists
+    if (!this.subscriptions[topicName]) {
+      // Combine topic name with subscription name to allow a worker building multi subscriptions.
+      this.subscriptions[topicName] = await this.pubsub
+        .createOrGetSubscription(topicName, `${topicName}-${subscriptionName}`, this.config);
+    }
+
+    return this.subscriptions[topicName];
+  }
+
+  async process(
+    jobName,
+    // eslint-disable-next-line no-unused-vars
+    callback = (jobData = {}, done = () => { }) => { },
+  ) {
+    // Create a job queue name(topic) with topic suffix for preventing naming conflic.
+    const topicName = `${jobName}-${topicSuffix}`;
+    const subscription = await this.getSubscription(topicName);
+
+    subscription.on('message', (message) => {
+      const doneCallback = () => {
+        logger.log(`The job of ${topicName} is finished and submit the ack request`, { message });
+
+        // Since the message ack not support promise for now (google/pubsub repo WIP).
+        // We have no way to know the exactly time when the ack job done.
+        message.ack();
+      };
+      callback(message.attributes, doneCallback);
+    });
+    subscription.on('error', (error) => {
+      logger.error(`The job of ${topicName} failed at ${moment().utc()}`, error);
+      this.emit('error', error);
+    });
+  }
+}
+
+const worker = new Worker();
+
+module.exports = { default: worker, Worker };
