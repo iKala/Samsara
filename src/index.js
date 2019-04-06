687e53983091d2b37daa243c00a22f27042a92c9
Calvin Huang <calvin.peak@capslock.tw>
2019-04-06 17:34:02 +0800

    lint: correct the arrow function lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Job = require('./job');
 const Worker = require('./worker');
 
-module.exports = (config) => ({
+module.exports = config => ({
   createJob: (name, data) => new Job({ name, data }, config),
   createWorker: () => new Worker(config),
 });
