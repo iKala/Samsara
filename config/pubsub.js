1f2269f2ef808f02ffbc31b8332fbeaa4cbc7574
Calvin Huang <calvin.peak@capslock.tw>
2019-04-06 15:55:18 +0800

    fix(test): ensure the random generated resource name follows the naming rule

--- config/pubsub.js
+++ config/pubsub.js
@@ -9,7 +9,7 @@ const randomize = require('randomatic');
 // Automatic generate a topic suffix when the specific suffix not exists.
 const hasPubsubTopicSuffixEnv = !!process.env.GOOGLE_PUBSUB_TOPIC_SUFFIX;
 if (!hasPubsubTopicSuffixEnv) {
-  const topicSuffix = `${randomize('Aa0', 10)}-${randomize('Aa0', 4)}`;
+  const topicSuffix = `${randomize('Aa', 1)}${randomize('Aa0', 9)}-${randomize('Aa0', 4)}`;
   fs.appendFileSync('.env', `\nGOOGLE_PUBSUB_TOPIC_SUFFIX=${topicSuffix}`);
 
   // Prepare the topic name suffix.
@@ -18,7 +18,7 @@ if (!hasPubsubTopicSuffixEnv) {
 
 const hasPubsubSubscriptionNameEnv = !!process.env.GOOGLE_PUBSUB_SUBSCRIPTION_NAME;
 if (!hasPubsubSubscriptionNameEnv) {
-  const subcriptionName = `${randomize('Aa0', 10)}-${randomize('Aa0', 4)}`;
+  const subcriptionName = `${randomize('Aa', 1)}${randomize('Aa0', 9)}-${randomize('Aa0', 4)}`;
   fs.appendFileSync('.env', `\nGOOGLE_PUBSUB_SUBSCRIPTION_NAME=${subcriptionName}`);
 
   // Prepare the topic name suffix.
