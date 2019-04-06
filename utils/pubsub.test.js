f1558c9a999fa2a708d13d97e9b2fecffa071e9b
Calvin Huang <calvin.peak@capslock.tw>
2019-04-06 16:05:36 +0800

    fix(test): ensure the test resource would be removed

--- utils/pubsub.test.js
+++ utils/pubsub.test.js
@@ -1,3 +1,5 @@
+require('module-alias/register');
+
 /**
  * Module dependencies
  */
@@ -6,23 +8,39 @@ const { Topic, Subscription } = require('@google-cloud/pubsub');
 /**
  * Configurations
  */
-const { topicSuffix, subscriptionName } = require('~config/pubsub');
-const pubsub = require('./pubsub');
+const { credentials, topicSuffix, subscriptionName } = require('~config/pubsub');
+const PubSub = require('./pubsub');
+
+const pubsub = new PubSub({ credentials });
 
 describe('pubsub (instance of ExtendedPubSub)', () => {
-  jest.setTimeout(20000);
+  jest.setTimeout(200000);
+
+  const resetAll = async () => {
+    const [subscriptions] = await pubsub.getSubscriptions();
+    const matchedSubscriptions = subscriptions
+      .filter(subscription => subscription.name.endsWith(subscriptionName));
+
+    await Promise.all(
+      matchedSubscriptions
+        .map(subscription => {
+          return subscription.delete();
+        }),
+    )
+
+    const [topics] = await pubsub.getTopics();
+    const matchedTopics = topics
+      .filter(topic => topic.name.endsWith(topicSuffix));
+
+    await Promise.all(
+      matchedTopics
+        .map(topic => topic.delete()),
+    )
+  };
 
   const topicName = `test-${topicSuffix}`;
-  afterEach(async () => {
-    const [subscriptions] = await pubsub.getSubscriptions();
-    if (subscriptions.find(subscription => subscription.name.endsWith(subscriptionName))) {
-      await pubsub.subscription(subscriptionName).delete();
-    }
-    const [topics] = await pubsub.getTopics();
-    if (topics.find(topic => topic.name.endsWith(topicName))) {
-      await pubsub.topic(topicName).delete();
-    }
-  });
+  beforeEach(resetAll);
+  afterEach(resetAll);
 
   describe('.createTopicIfNotExists', () => {
     test('the topic should be create when not exists', async () => {
@@ -38,6 +56,7 @@ describe('pubsub (instance of ExtendedPubSub)', () => {
 
       await pubsub.createTopic(topicName);
       [topics] = await pubsub.getTopics();
+
       // Ensure the topic created.
       expect(topics.find(topic => topic.name.endsWith(topicName))).toEqual(expect.anything());
 
@@ -106,7 +125,7 @@ describe('pubsub (instance of ExtendedPubSub)', () => {
       expect(subscription).toBeInstanceOf(Subscription);
     });
 
-    test('it shoule return the subscrition', async () => {
+    test('it shoule return the subscription', async () => {
       await pubsub.createTopic(topicName);
       await pubsub.topic(topicName).createSubscription(subscriptionName);
 
