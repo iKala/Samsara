245539077a4035af0af4d4cddbd62cea42d35058
Calvin Huang <calvin.peak@capslock.tw>
2020-04-24 20:08:23 +0800

    fix: migrate to now subscription setting

--- utils/pubsub.js
+++ utils/pubsub.js
@@ -47,19 +47,7 @@ class ExtendedPubSub extends PubSub {
   async createOrGetSubscription(topicOrName, name, options = {}) {
     await this.createSubscriptionIfNotExists(topicOrName, name, options);
 
-    const topic = await (() => {
-      if (_.isString(topicOrName)) {
-        return this.topic(topicOrName);
-      }
-
-      if (topicOrName instanceof Topic) {
-        return topicOrName;
-      }
-
-      throw new Error('topicOrName must be string or topic instance.');
-    })();
-
-    return topic.subscription(name, options);
+    return this.subscription(name, options);
   }
 }
 
