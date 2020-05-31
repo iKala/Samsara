b8f664178ea420a6e1968f4addf1aa0e11c205d7
Calvin Huang <calvin.peak@capslock.tw>
2020-06-01 00:21:01 +0800

    Revert "fix: use v1 publish client to send message"
    This reverts commit c6b643b6234e2e3e96d33e1166e3fb3a5f9fae20.

--- src/job.js
+++ src/job.js
@@ -4,24 +4,26 @@
  */
 const _ = require('lodash');
 const moment = require('moment');
-const { v1 } = require('@google-cloud/pubsub');
 const grpc = require('grpc');
 
 /**
  * Utilities
  */
+const PubSub = require('../utils/pubsub');
 const Logger = require('../utils/logger');
 
 /**
  * Configurations
  */
 
+const defaultMaxRetries = 200;
+
 class Job {
   constructor({ name, data = {} }, config = {}) {
     this.name = name;
     this.data = data;
 
-    const configWithDefaultValue = _.defaults(config);
+    const configWithDefaultValue = _.defaults(config, { maxRetries: defaultMaxRetries });
 
     this.config = configWithDefaultValue;
 
@@ -31,19 +33,14 @@ class Job {
       throw new Error('`credentials` is required for setting up the Google Cloud Pub/Sub');
     }
 
+    this.pubsub = new PubSub({ credentials, projectId, grpc });
     this.logger = new Logger({ debug: configWithDefaultValue.debug });
-
-    this.publisher = new v1.PublisherClient({ credentials, projectId, grpc });
   }
 
   async save() {
-    const { projectId, topicSuffix, batching = {} } = this.config;
+    const { topicSuffix, batching = {} } = this.config;
     const topicName = `${this.name}-${topicSuffix}`;
-
-    const formattedTopic = this.publisher.topicPath(
-      projectId,
-      topicName,
-    );
+    const topic = await this.pubsub.createOrGetTopic(topicName, { batching });
 
     const dataBuffer = Buffer.from(
       JSON.stringify({
@@ -53,42 +50,28 @@ class Job {
       }),
     );
 
-    const messagesElement = {
-      data: dataBuffer,
-    };
-    const messages = [messagesElement];
+    let latestError;
+    let retry = -1;
+    const maxRetries = this.config.maxRetries || defaultMaxRetries;
 
-    // Build the request
-    const request = {
-      topic: formattedTopic,
-      messages: messages,
-    };
+    do {
+      try {
+        const message = await topic.publish(dataBuffer);
+        this.logger.log(`The job created on the ${topicName}`, { data: this.data, dataBuffer });
+        return message;
+      } catch (error) {
+        latestError = error;
+        retry += 1;
+        this.logger.log(`🔄 Retry ${retry}/${maxRetries}`);
+      }
 
-    const retrySettings = {
-      retryCodes: [
-        10, // 'ABORTED'
-        1, // 'CANCELLED',
-        4, // 'DEADLINE_EXCEEDED'
-        13, // 'INTERNAL'
-        8, // 'RESOURCE_EXHAUSTED'
-        14, // 'UNAVAILABLE'
-        2, // 'UNKNOWN'
-      ],
-      backoffSettings: {
-        initialRetryDelayMillis: 100,
-        retryDelayMultiplier: 1.3,
-        maxRetryDelayMillis: 60000,
-        initialRpcTimeoutMillis: 5000,
-        rpcTimeoutMultiplier: 1.0,
-        maxRpcTimeoutMillis: 600000,
-        totalTimeoutMillis: 600000,
-      },
-    };
+      if (retry > maxRetries) {
+        retry = -1;
+        this.logger.log('❌ Retry too much time.');
+      }
+    } while (retry !== -1);
 
-    return await this.publisher.publish(request, {
-      retry: retrySettings,
-      batching,
-    });
+    throw latestError;
   }
 }
 
